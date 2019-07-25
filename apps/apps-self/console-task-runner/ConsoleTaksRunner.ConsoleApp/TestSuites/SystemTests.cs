754d8bd75e7423456c68ffced763d3fcf6753646
teoman shipahi <aytekoz@yahoo.com>
2019-07-25 12:47:15 -0400

    public void GetEnvironmentProperties(ApplicationEnvironment applicationEnvironment)

---
+++ apps/apps-self/console-task-runner/ConsoleTaksRunner.ConsoleApp/TestSuites/SystemTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleTaksRunner.ConsoleApp.TestSuites
+{
+    public class SystemTests : ITestSuite
+    {
+        public void GetEnvironmentProperties(ApplicationEnvironment applicationEnvironment)
+        {
+            TestUtilities.ConsoleWriteJson(new
+            {
+                Environment.CommandLine,
+                Environment.CurrentDirectory,
+                Environment.CurrentManagedThreadId,
+                Environment.ExitCode,
+                Environment.HasShutdownStarted,
+                Environment.Is64BitOperatingSystem,
+                Environment.Is64BitProcess,
+                Environment.MachineName,
+                Environment.NewLine,
+                Environment.OSVersion,
+                Environment.ProcessorCount,
+                Environment.SystemDirectory,
+                Environment.TickCount,
+                Environment.SystemPageSize,
+                Environment.UserDomainName,
+                Environment.UserName,
+                Environment.Version,
+                Environment.UserInteractive,
+                Environment.WorkingSet,
+                LogicalDrives = Environment.GetLogicalDrives(),
+                Environment.SpecialFolder.Desktop,
+                Environment.SpecialFolder.UserProfile,
+            });
+        }
+    }
+}
