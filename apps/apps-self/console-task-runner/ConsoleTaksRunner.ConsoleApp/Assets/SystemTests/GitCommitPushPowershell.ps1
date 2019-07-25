24359da6545d6374e83095fd4a94d7a4cc17cc89
teoman shipahi <aytekoz@yahoo.com>
2019-07-25 18:26:55 -0400

    {{Title}}
    {{Description}}

--- apps/apps-self/console-task-runner/ConsoleTaksRunner.ConsoleApp/Assets/SystemTests/GitCommitPushPowershell.ps1
+++ apps/apps-self/console-task-runner/ConsoleTaksRunner.ConsoleApp/Assets/SystemTests/GitCommitPushPowershell.ps1
@@ -1,3 +1,4 @@
 ﻿git add "C:\Github\Ggg.Github\Ggg.Csharp\apps\apps-self\console-task-runner\ConsoleTaksRunner.ConsoleApp\*"
 git commit "C:\Github\Ggg.Github\Ggg.Csharp\apps\apps-self\console-task-runner\ConsoleTaksRunner.ConsoleApp\*" -m "{{Title}}" -m "{{Description}}"
 git push
+pause
