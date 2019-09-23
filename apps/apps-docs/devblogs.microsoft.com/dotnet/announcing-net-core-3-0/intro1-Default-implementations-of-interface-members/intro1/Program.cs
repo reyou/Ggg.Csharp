f54168eda81279b87381cef6d73497be6a447c8c
aytekin ozdemir <aytekoz@yahoo.com>
2019-09-23 16:32:36 -0500

    Default implementations of interface members

---
+++ apps/apps-docs/devblogs.microsoft.com/dotnet/announcing-net-core-3-0/intro1-Default-implementations-of-interface-members/intro1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace intro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    interface ILogger
+    {
+        void Log(LogLevel level, string message);
+        void Log(Exception ex) => Log(LogLevel.Error, ex.ToString()); // New overload
+    }
+
+    internal enum LogLevel
+    {
+        Error
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void Log(LogLevel level, string message)
+        {
+
+        }
+        // Log(Exception) gets default implementation
+    }
+
+}
