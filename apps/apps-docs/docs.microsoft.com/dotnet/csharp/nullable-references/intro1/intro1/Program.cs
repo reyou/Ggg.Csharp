3ed7498ee7c9eef047f2ff684393fac03ebc709d
aytekin ozdemir <aytekoz@yahoo.com>
2019-09-23 16:26:06 -0500

    nullable

---
+++ apps/apps-docs/docs.microsoft.com/dotnet/csharp/nullable-references/intro1/intro1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace intro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // nullable string variable
+            string? name;
+
+            // Console.WriteLine(name!.Length);
+        }
+    }
+}
