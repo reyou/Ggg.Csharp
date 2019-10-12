6b1f14eb19313d23583fd9b344eaa3e44c34091e
aytekin ozdemir <aytekoz@yahoo.com>
2019-10-12 15:51:56 -0500

    document db

---
+++ apps/apps-docs/docs.microsoft.com/azure/cosmos-db/how-to-use-resource-tokens-gremlin/intro1-create-a-resource-token/intro1/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace intro1
+{
+    /// <summary>
+    /// https://www.nuget.org/packages/Microsoft.Azure.DocumentDB/
+    /// https://docs.microsoft.com/en-us/azure/cosmos-db/how-to-use-resource-tokens-gremlin#create-a-resource-token
+    /// </summary>
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ConnectionPolicy connectionPolicy = new ConnectionPolicy
+            {
+                EnableEndpointDiscovery = false,
+                ConnectionMode = ConnectionMode.Direct
+            };
+            // Uri serviceEndpoint = new Uri("https://contoso.documents.azure.com:443/");
+            string hostname = File.ReadAllText(@"C:\apikeys\cosmos-db-gremlin\hostname.txt");
+            Uri serviceEndpoint = new Uri(hostname);
+            string authKey = File.ReadAllText(@"C:\apikeys\cosmos-db-gremlin\authKey.txt");
+            DocumentClient client = new DocumentClient(serviceEndpoint, authKey, connectionPolicy);
+            // Read specific permission to obtain a token.
+            // The token isn't returned during the ReadPermissionReedAsync() call.
+            // The call succeeds only if database id, user id, and permission id already exist. 
+            // Note that <database id> is not a database name. It is a base64 string that represents the database identifier, for example "KalVAA==".
+            // Similar comment applies to <user id> and <permission id>.
+            try
+            {
+                Permission permission = await client.ReadPermissionAsync(UriFactory.CreatePermissionUri("<database id>", "<user id>", "<permission id>"));
+                Console.WriteLine("Obtained token {0}", permission.Token);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ apps/apps-docs/docs.microsoft.com/azure/cosmos-db/how-to-use-resource-tokens-gremlin/intro2-use-a-resource-token/intro1/Program.cs
@@ -0,0 +1,44 @@
+﻿using Gremlin.Net.Driver;
+using Gremlin.Net.Structure.IO.GraphSON;
+using System;
+using System.Threading.Tasks;
+
+namespace intro1
+{
+    /// <summary>
+    /// https://www.nuget.org/packages/Microsoft.Azure.DocumentDB/
+    /// https://docs.microsoft.com/en-us/azure/cosmos-db/how-to-use-resource-tokens-gremlin#use-a-resource-token
+    /// </summary>
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // The Gremlin application needs to be given a resource token. It can't discover the token on its own.
+            // You can obtain the token for a given permission by using the Azure Cosmos DB SDK, or you can pass it into the application as a command line argument or configuration value.
+            string resourceToken = GetResourceToken();
+
+            // Configure the Gremlin server to use a resource token rather than a master key.
+            GremlinServer server = new GremlinServer(
+                "contoso.gremlin.cosmosdb.azure.com",
+                port: 443,
+                enableSsl: true,
+                username: "/dbs/<database name>/colls/<collection name>",
+
+                // The format of the token is "type=resource&ver=1&sig=<base64 string>;<base64 string>;".
+                password: resourceToken);
+
+            using (GremlinClient gremlinClient = new GremlinClient(server, new GraphSON2Reader(), new GraphSON2Writer(), GremlinClient.GraphSON2MimeType))
+            {
+                await gremlinClient.SubmitAsync("g.V().limit(1)");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static string GetResourceToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
