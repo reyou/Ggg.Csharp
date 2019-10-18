86f12b1c638bee3f7df8cc6416b6d6d3421a1cfa
aytekin ozdemir <aytekoz@yahoo.com>
2019-10-18 16:45:31 -0500

    intro1-multiple-httpRequests

---
+++ apps/apps-docs/docs.microsoft.com/dotnet/api/system.threading.tasks.task.waitall/intro1-multiple-httpRequests/intro1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace intro1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            HttpClient client = new HttpClient();
+            int count = 10000;
+            List<Task<HttpResponseMessage>> tasks = new List<Task<HttpResponseMessage>>();
+            for (int i = 0; i < count; i++)
+            {
+                Task<HttpResponseMessage> task = client.GetAsync("http://www.example.com");
+                tasks.Add(task);
+            }
+
+            HttpResponseMessage[] httpResponseMessages = await Task.WhenAll(tasks);
+            int counter = 0;
+            foreach (HttpResponseMessage httpResponseMessage in httpResponseMessages)
+            {
+                string readAsStringAsync = await httpResponseMessage.Content.ReadAsStringAsync();
+                Console.WriteLine(readAsStringAsync);
+                Console.WriteLine($"Fetched: {++counter}");
+            }
+
+            Console.WriteLine("Bye World!");
+            Console.ReadLine();
+        }
+    }
+}
