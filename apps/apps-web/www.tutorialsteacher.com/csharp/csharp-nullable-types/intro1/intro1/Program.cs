e95dde1a6a316131df4c9fdea98c439fb21241a8
Aytekin Ozdemir <aytekoz@yahoo.com>
2019-10-23 08:58:36 -0500

    GetValueOrDefault

---
+++ apps/apps-web/www.tutorialsteacher.com/csharp/csharp-nullable-types/intro1/intro1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace intro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GetValueOrDefault();
+            Console.WriteLine("Bye World!");
+            Console.ReadLine();
+        }
+
+        private static void GetValueOrDefault()
+        {
+            int? i = null;
+            double? D = null;
+            if (i.HasValue)
+            {
+                Console.WriteLine(i.Value);
+            }
+            Console.WriteLine(i.GetValueOrDefault());
+        }
+    }
+}
