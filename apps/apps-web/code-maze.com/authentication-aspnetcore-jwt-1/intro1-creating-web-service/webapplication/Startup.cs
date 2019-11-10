a506e36f94a506026e67d12562187b508131ea22
Teoman Shipahi <aytekoz@yahoo.com>
2019-11-10 00:20:27 -0600

    ASP.NET Core Authentication with JWT and Angular – Part 2

---
+++ apps/apps-web/code-maze.com/authentication-aspnetcore-jwt-1/intro1-creating-web-service/webapplication/Startup.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace webapplication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(ConfigureAuthenticationOptions)
+                .AddJwtBearer(ConfigureTokenValidationParameters);
+            services.AddControllers();
+        }
+
+        private void ConfigureTokenValidationParameters(JwtBearerOptions options)
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = "http://localhost:5000",
+                ValidAudience = "http://localhost:5000",
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"))
+            };
+        }
+
+        private void ConfigureAuthenticationOptions(AuthenticationOptions configureOptions)
+        {
+            configureOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            configureOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
