a553dae0813cac9264bc6fa3ecc9cb76432c85ac
Teoman Shipahi <aytekoz@yahoo.com>
2019-11-15 00:55:34 -0600

    \intro3-modify-realBackendProvider

---
+++ apps/apps-web/jasonwatmore.com/angular-8-jwt-authentication-example-tutorial/intro3-modify-realBackendProvider/angular-8-jwt-authentication-example/src/app/app.module.ts
@@ -0,0 +1,36 @@
+﻿import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+
+// used to create fake backend
+import { fakeBackendProvider } from "./_helpers";
+
+import { AppComponent } from "./app.component";
+import { appRoutingModule } from "./app.routing";
+
+import { JwtInterceptor, ErrorInterceptor } from "./_helpers";
+import { HomeComponent } from "./home";
+import { LoginComponent } from "./login";
+
+@NgModule({
+  // makes the exported declarations of other modules available in the current module.
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    appRoutingModule
+  ],
+  // “declarations” is for things you’ll use in your templates: mainly components
+  declarations: [AppComponent, HomeComponent, LoginComponent],
+  // Providers are to make services and values known to DI (dependency injection).
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
