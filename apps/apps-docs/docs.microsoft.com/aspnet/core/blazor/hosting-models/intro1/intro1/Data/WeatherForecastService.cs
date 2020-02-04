d96d94d7b5cbed37f0f8830884e3eba4eab38107
aytekin ozdemir <aytekin.ozdemir@blackbaud.me>
2020-02-04 12:55:48 -0600

    blazor.server.js

--- apps/apps-docs/docs.microsoft.com/aspnet/core/blazor/hosting-models/intro1/intro1/Data/WeatherForecastService.cs
+++ apps/apps-docs/docs.microsoft.com/aspnet/core/blazor/hosting-models/intro1/intro1/Data/WeatherForecastService.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Caching.Memory;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,20 +7,39 @@ namespace intro1.Data
 {
     public class WeatherForecastService
     {
-        private static readonly string[] Summaries = new[]
+        private static readonly string[] _summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
+        public IMemoryCache MemoryCache { get; }
+
+        public WeatherForecastService(IMemoryCache memoryCache)
+        {
+            MemoryCache = memoryCache;
+        }
 
         public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
-            var rng = new Random();
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            Task<WeatherForecast[]> weatherForecasts = MemoryCache.GetOrCreateAsync(startDate, async cacheEntry =>
             {
-                Date = startDate.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            }).ToArray());
+                cacheEntry.SetOptions(new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow =
+                        TimeSpan.FromSeconds(30)
+                });
+
+                Random rng = new Random();
+
+                await Task.Delay(TimeSpan.FromSeconds(10));
+
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = startDate.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = _summaries[rng.Next(_summaries.Length)]
+                }).ToArray();
+            });
+            return weatherForecasts;
         }
     }
 }
