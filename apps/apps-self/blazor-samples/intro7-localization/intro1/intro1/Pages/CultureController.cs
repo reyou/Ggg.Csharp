81bc4ee70f2b5818f11cb7684a7a7a04003433ff
Aytekin Ozdemir <aytekoz@yahoo.com>
2020-12-30 15:56:17 -0600

    ASP.NET Core Razor components class libraries

---
+++ apps/apps-self/blazor-samples/intro7-localization/intro1/intro1/Pages/CultureController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Localization;
+
+namespace intro1.Pages
+{
+    [Route("[controller]/[action]")]
+    public class CultureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SetCulture(string culture, string redirectUri)
+        {
+            if (culture != null)
+            {
+                HttpContext.Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)));
+            }
+            return LocalRedirect(redirectUri);
+        }
+    }
+}
---
+++ apps/apps-self/blazor-samples/intro7-localization/intro1/intro1/Pages/_Host.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace intro1.Pages
+{
+    public class HostModel : PageModel
+    {
+        public void OnGet()
+        {
+            HttpContext.Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(CultureInfo.CurrentCulture, CultureInfo.CurrentUICulture)));
+        }
+    }
+}
