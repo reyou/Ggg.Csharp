c1966684f8d47082976431064f1016e1aa73d0cc
Aytekin Ozdemir <aytekoz@yahoo.com>
2021-02-26 12:51:15 -0600

    filters

---
+++ apps/apps-self/exception-filter-example/intro1/intro1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using intro1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace intro1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy(string argumentParam)
+        {
+            if (string.IsNullOrEmpty(argumentParam))
+            {
+                throw new ArgumentNullException(nameof(argumentParam));
+            }
+
+            return View(argumentParam);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ apps/apps-self/exception-filter-example/intro1/intro1/CustomExceptionFilter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Hosting;
+
+namespace intro1
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        private readonly IModelMetadataProvider _modelMetadataProvider;
+
+        public CustomExceptionFilter(IWebHostEnvironment hostingEnvironment, IModelMetadataProvider modelMetadataProvider)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            _modelMetadataProvider = modelMetadataProvider;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            if (!_hostingEnvironment.IsDevelopment())
+            {
+                return;
+            }
+
+            ViewResult result = new ViewResult
+            {
+                ViewName = "CustomError",
+                ViewData = new ViewDataDictionary(_modelMetadataProvider, context.ModelState)
+                {
+                    {"Exception", context.Exception}
+                }
+            };
+            
+            // TODO: Pass additional detailed data via ViewData
+            context.Result = result;
+        }
+    }
+}
