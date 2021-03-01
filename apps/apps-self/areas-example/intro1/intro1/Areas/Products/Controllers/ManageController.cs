546722f86d7fa281a3f48a0a9f9064e70bb2d662
Aytekin Ozdemir <aytekoz@yahoo.com>
2021-03-01 11:19:10 -0600

    Areas (checkout, billing, and search)

---
+++ apps/apps-self/areas-example/intro1/intro1/Areas/Products/Controllers/ManageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace intro1.Areas.Products.Controllers
+{
+    [Area("Products")]
+    public class ManageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
